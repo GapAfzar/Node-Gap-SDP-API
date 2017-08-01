ed1b44c3b846bb4cb352677c970db20f26dd6608
amirkheirabadi <amirkheirabadi.com@gmail.com>
2017-08-01 11:50:45 +0430

    update API for new features

--- gap.js
+++ gap.js
@@ -1,200 +1,255 @@
 "use strict";
 
 var express = require('express'),
-bodyParser = require('body-parser'),
-request = require('request'),
-fs = require('fs');
+    bodyParser = require('body-parser'),
+    request = require('request'),
+    fs = require('fs');
+
+const noop = () => {}
 
 module.exports = class GapApi {
-	constructor(token, options = {}) {
-		this.apiUrl = 'https://api.gap.im/';
-		this.token = token;
-		this.port = options.port || 3000;
-		this.joinHandler = () => {};
-		this.mainHandler = () => {};
-		this.triggerButtonHandler = () => {};
-		this.textHandlers = [];
-		this.startHttpServer();
-	}
+  constructor(token, options) {
+    options = options || {}
+    this.apiUrl = 'https://api.gap.im/';
+    this.token = token;
+    this.port = options.port || 3000;
+    this.joinHandler = noop;
+    this.mainHandler = noop;
+    this.triggerButtonHandler = noop;
+    this.submitFormHandler = noop;
+    this.paidHandler = noop;
+    this.textHandlers = [];
+    this.startHttpServer();
+  }
 
-	startHttpServer() {
-		var app = express();
-		var self = this;
-		app.use(bodyParser.json());
-		app.use(bodyParser.urlencoded({
-			extended: true
-		}));
-		app.post('*', function (req, res) {
-			self._onText(req, res);
-		});
-		app.listen(this.port);
-	}
+  startHttpServer() {
+    var app = express();
+    var self = this;
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+      extended: true
+    }));
+    app.use(function(req, res) {
+      self._onText(req, res);
+    });
+    app.listen(this.port);
+  }
 
-	onJoin(callback = () => {}) {
-		this.joinHandler = callback;
-	}
+  onJoin(callback) {
+    this.joinHandler = callback || noop;
+  }
 
-	onTriggerButton(callback) {
-		this.triggerButtonHandler = callback || function() {};
-	}
+  onTriggerButton(callback) {
+    this.triggerButtonHandler = callback || noop
+  }
 
-	onText() {
-		switch (arguments.length) {
-			case 1 :
-				this.mainHandler = arguments[0] || function() {};
-			break;
-			case 2:
-				this.textHandlers.push({
-					regexp: arguments[0],
-					callback: arguments[1] || function() {}
-				});
-			break;
-		}
-		return true;
-	}
+  onSubmitForm(callback) {
+    this.submitFormHandler = callback || noop
+  }
 
-	_onText(req, res) {
-		if (req.body.type == 'join') {
-			this.joinHandler(req.body);
-		} else if (req.body.type == 'triggerButton') {
-			this.triggerButtonHandler(req.body);
-    	} else {
-			this.mainHandler(req.body);
-			if (this.textHandlers.length) {
-				this.findTextHandler(req.body);
-			}
-		}
-		res.send();
-	}
+  onPaid(callback) {
+    this.paidHandler = callback || noop
+  }
 
-	findTextHandler(data) {
-		var text = data.data || '';
-		if (!text.length || data.type != 'text') {
-			return true;
-		}
+  onText() {
+    switch (arguments.length) {
+    case 1:
+      this.mainHandler = arguments[0] ||
+      function() {};
+      break;
+    case 2:
+      this.textHandlers.push({
+        regexp: arguments[0],
+        callback: arguments[1] ||
+        function() {}
+      });
+      break;
+    }
+    return true;
+  }
 
-		this.textHandlers.every(function(handler) {
-			if (handler.regexp.test(text)) {
-				handler.callback(data);
-				return false;
-			}
-			return true;
-		});
-	}
+  _onText(req, res) {
+    if (req.body.type == 'join') {
+      this.joinHandler(req.body);
+    } else if (req.body.type == 'triggerButton') {
+      this.triggerButtonHandler(req.body);
+    } else if (req.body.type == 'submitForm') {
+      this.submitFormHandler(req.body);
+    } else if (req.body.type == 'paycallback') {
+      var params = JSON.parse(req.body.data);
+      this.paidHandler(params);
+    } else {
+      this.mainHandler(req.body);
+      if (this.textHandlers.length) {
+        this.findTextHandler(req.body);
+      }
+    }
+    res.send();
+  }
 
-	sendText(chatId, data, replyKeywords, inlineKeywords) {
-		var params = {chat_id: chatId, data: data};
-		if (replyKeywords) {
-			params['reply_keyboard'] = this._replyKeyboard(replyKeywords);
-		}
-		if (inlineKeywords) {
-			params['inline_keyboard'] = JSON.stringify(inlineKeywords)
-		}
-		return this._sendRequest('text', params);
-	}
+  findTextHandler(data) {
+    var text = data.data || '';
+    if (!text.length || data.type != 'text') {
+      return true;
+    }
 
-	sendImage(chatId, img, desc = '', replyKeywords = null) {
-		if (!fs.existsSync(img)) {
-		    throw "Image path is invalid";
-		};
+    this.textHandlers.every(function(handler) {
+      if (handler.regexp.test(text)) {
+        handler.callback(data);
+        return false;
+      }
+      return true;
+    });
+  }
 
-		var self = this;
-		this._uploadFile('image', img, (data) => {
-		    data['desc'] = desc;
-		    let params = {chat_id: chatId, data: JSON.stringify(data)};
-		    if (replyKeywords) {
-		        params['reply_keyboard'] = self._replyKeyboard(replyKeywords);
-		    }
-		    self._sendRequest('image', params);
-		});
-	}
+  sendText(chatId, data, replyKeywords, inlineKeywords, form) {
+    var params = {
+      chat_id: chatId,
+      data: data
+    };
+    if (replyKeywords) {
+      params['reply_keyboard'] = this._replyKeyboard(replyKeywords);
+    }
+    if (inlineKeywords) {
+      params['inline_keyboard'] = JSON.stringify(inlineKeywords)
+    }
+    if (form) {
+      params['form'] = JSON.stringify(form)
+    }
+    return this._sendRequest('text', params);
+  }
 
-	sendVideo(chatId, video, desc = '', replyKeywords = null) {
-		if (!fs.existsSync(video)) {
-		    throw "Video path is invalid";
-		};
+  sendImage(chatId, img, desc, replyKeywords) {
+    if (!fs.existsSync(img)) {
+      throw "Image path is invalid";
+    };
 
-		var self = this;
-		this._uploadFile('video', video, (data) => {
-		    data['desc'] = desc;
-		    let params = {chat_id: chatId, data: JSON.stringify(data)};
-		    if (replyKeywords) {
-		        params['reply_keyboard'] = self._replyKeyboard(replyKeywords);
-		    }
-		    self._sendRequest('video', params);
-		});
-	}
+    var self = this;
+    this._uploadFile(img, (data) => {
+      data['desc'] = desc;
+      let params = {
+        chat_id: chatId,
+        data: JSON.stringify(data)
+      };
+      if (replyKeywords) {
+        params['reply_keyboard'] = self._replyKeyboard(replyKeywords);
+      }
+      self._sendRequest('image', params);
+    });
+  }
 
-	editText(chatId, messageId, data, inlineKeywords) {
-		var params = {
-			chat_id: chatId,
-			message_id: messageId
-		};
-		if (data) {
-			params['data'] = data
-		}
-		if (inlineKeywords) {
-			params['inline_keyboard'] = JSON.stringify(inlineKeywords)
-		}
-		return this._sendRequest(false, params, 'editMessage');
-	}
+  sendVideo(chatId, video, desc, replyKeywords) {
+    if (!fs.existsSync(video)) {
+      throw "Video path is invalid";
+    };
 
-	answerCallback(chatId, callback_id, text, show_alert) {
-		var params = {
-			chat_id: chatId,
-			callback_id,
-			text,
-			show_alert
-		};
-		return this._sendRequest(false, params, 'answerCallback');
-	}
+    var self = this;
+    this._uploadFile(video, (data) => {
+      data['desc'] = desc;
+      let params = {
+        chat_id: chatId,
+        data: JSON.stringify(data)
+      };
+      if (replyKeywords) {
+        params['reply_keyboard'] = self._replyKeyboard(replyKeywords);
+      }
+      self._sendRequest('video', params);
+    });
+  }
 
-	_uploadFile(type, address, callback = () => {}) {
-		var formData = {};
-		formData[type] = fs.createReadStream(address);
+  editText(chatId, messageId, data, inlineKeywords) {
+    var params = {
+      chat_id: chatId,
+      message_id: messageId
+    };
+    if (data) {
+      params['data'] = data
+    }
+    if (inlineKeywords) {
+      params['inline_keyboard'] = JSON.stringify(inlineKeywords)
+    }
+    return this._sendRequest(false, params, 'editMessage');
+  }
 
-		request.post({url:this.apiUrl + 'upload', formData: formData}, (err, httpResponse, body) => {
-		    if (err) {
-		        return console.error('upload failed:', err);
-		    }
-		    callback(JSON.parse(body));
-		});
-	}
+  deleteMessage(chatId, messageId) {
+    var params = {
+      chat_id: chatId,
+      message_id: messageId
+    };
+    return this._sendRequest(false, params, 'deleteMessage');
+  }
 
-	_replyKeyboard(keyboard, once = true, selective = false) {
-		if (!keyboard instanceof Array) {
-		    throw "keyboard must be array";
-		}
-		
-		let replyKeyboard = {
-		    keyboard: keyboard,
-		    once: once,
-		    selective: selective,
-		};
+  answerCallback(chatId, callback_id, text, show_alert) {
+    var params = {
+      chat_id: chatId,
+      callback_id,
+      text,
+      show_alert
+    };
+    return this._sendRequest(false, params, 'answerCallback');
+  }
 
-		return JSON.stringify(replyKeyboard);
-	}
+  payVerify(chatId, ref_id) {
+    var params = {
+      chat_id: chatId,
+      ref_id,
+    };
+    return this._sendRequest(false, params, 'payVerify');
+  }
 
-	_sendRequest(msgType, params, method = 'sendMessage') {
-		if (msgType) {
-			params['type'] = msgType;
-		}
-		var options = {
-			url: this.apiUrl + method,
-			method: 'post',
-			form: params,
-			headers: {
-				'token': this.token
-			}
-		};
-		request(options, (error, response, body) => {
-			if (error || response.statusCode != 200) {
-				if (body) {
-					throw body;
-				}
-				throw 'an error was encountered';
-			}
-			return true;
-		});
-	}
+  _uploadFile(address, callback) {
+    callback = callback || noop
+    var formData = {
+      file: fs.createReadStream(address),
+    };
+
+    request.post({
+      url: this.apiUrl + 'upload',
+      formData: formData
+    }, (err, httpResponse, body) => {
+      if (err) {
+        return console.error('upload failed:', err);
+      }
+      callback(JSON.parse(body));
+    });
+  }
+
+  _replyKeyboard(keyboard, once, selective) {
+    if (!keyboard instanceof Array) {
+      throw "keyboard must be array";
+    }
+
+    let replyKeyboard = {
+      keyboard: keyboard,
+      once: once || true,
+      selective: selective,
+    };
+
+    return JSON.stringify(replyKeyboard);
+  }
+
+  _sendRequest(msgType, params, method) {
+    if (msgType) {
+      params['type'] = msgType;
+    }
+    return new Promise((resolve, reject) => {
+      var options = {
+        url: this.apiUrl + (method || 'sendMessage'),
+        method: 'post',
+        form: params,
+        headers: {
+          'token': this.token
+        }
+      };
+      request(options, (error, response, body) => {
+        if (error || response.statusCode != 200) {
+          if (body) {
+            return reject(body);
+          }
+          return reject('an error was encountered');
+        }
+        return resolve(body);
+      });
+    })
+  }
 }
